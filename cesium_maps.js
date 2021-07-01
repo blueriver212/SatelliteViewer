11c424179ef2bccaef5c6807f25c201674750c62
blueriver212 <53503060+blueriver212@users.noreply.github.com>
2021-07-01 19:55:54 +0100

    Feature: Initial Commit

---
+++ cesium_maps.js
@@ -0,0 +1,23 @@
+function single_year () {
+    // Initialize the Cesium Viewer in the HTML element with the "cesiumContainer" ID.
+        const viewer = new Cesium.Viewer('cesiumContainer', {
+        terrainProvider: Cesium.createWorldTerrain()
+        });    
+
+        viewer.dataSources.add(
+        Cesium.GeoJsonDataSource.load(
+          "../USA.json",
+          {
+            stroke: Cesium.Color.HOTPINK,
+            fill: Cesium.Color.PINK.withAlpha(0.5),
+            strokeWidth: 3,
+          }
+        )
+      );
+}
+
+function two_year () {
+    const viewer_compare = new Cesium.Viewer('cesiumContainer', {
+        terrainProvider: Cesium.createWorldTerrain()
+        }); 
+}
