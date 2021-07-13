3cef191644c6514cd27c1d62adad0fc709ef6141
blueriver212 <53503060+blueriver212@users.noreply.github.com>
2021-07-13 18:23:23 +0100

    Feature: removing some old code

--- one_year.js
+++ one_year.js
@@ -1,3 +1,5 @@
+const { default: Cesium3DTile } = require("cesium/Source/Scene/Cesium3DTile");
+
 var satcat = new Catalogue(); // intialise the catalogue class
 
 function oneYearLoad() {
@@ -14,5 +16,11 @@ function main() {
     // here he has the the different types of loading, we are just going to first return the test data
 
     //satcat.loadcatlog("kep", "https://satellite-api.herokuapp.com/2019");
-    satcat.loadcatlog("kep", "http://satellite-api.herokuapp.com/2022"); // okay this now works
+    //satcat.loadcatlog("kep", "http://satellite-api.herokuapp.com/2022"); // okay this now works
+    // //viewer.dataSources.add(Cesium.GeoJsonDataSource.load('satellites_1.geojson'));
+    // var datasource = new Cesium.GeoJsonDataSource();
+    // var satellites = datasource.load('satellites_1.geojson');
+    // satellites.then(function(datasource) {
+  
+  
 }
