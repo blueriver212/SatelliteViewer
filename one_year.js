4cce4c2f09e0725d6e9b4fa850545d3289e47088
blueriver212 <53503060+blueriver212@users.noreply.github.com>
2021-07-19 15:10:53 +0100

    Fix: Solved the problem where dat wasnt removed when a new year was entered

--- one_year.js
+++ one_year.js
@@ -1,5 +1,3 @@
-const { countReset } = require("console");
-
 var viewer_main, radar_viewer;
 var start_jd;
 
@@ -9,26 +7,45 @@ var debris_collection;
 var debri_collection_radar; /// it is the same as debris_collection
 
 var satcat;
-var data_path="data/";
 
 
-function removeMapData () {
-    // viewer_main.debris_collection.removeAll();
-    // //debris_collection.removeAll();
-    // console.log('ive been clicked');
-    this.debris_kep=[];
-    this.data_load_complete=false;
+// function removeMapData () {
+    
+//     // //debris_collection.removeAll();
+//     // console.log('ive been clicked');
+//     // this.debris_kep=[];
+//     // this.data_load_complete=false;
+//    // console.log('im going to remove the mpa data');
+//     //viewer_main.scene.primitives.remove(debris_collection);
+//     //satcat.clear_catalog();
+//     //data_load = false;
+//     viewer_main.scene.postUpdate.removeEventListener(update_debris_position);
+//     viewer_main.scene.primitives.remove(debris_collection);
+
+//   }
+
+function numberOfLoads() {
+  count = count + 1; 
+  console.log(count);
+
+  if (count >= 2) {
+    // viewer_main.scene.postUpdate.removeEventListener(update_debris_position);
+    // viewer_main.scene.primitives.remove(debris_collection);
+    // oneYearLoad();
+      satcat.clear_catalog();
+      data_load = false;
+      debris_collection.removeAll();
+      debri_collection_radar.removeAll();
+      oneYearLoad();
+  } else {
+    oneYearLoad();
   }
 
+}
 function oneYearLoad() {
-    count = count + 1; 
-    console.log(count);
+
     satcat = new Catalogue();
 
-    if (count >= 2) {
-      removeMapData(); // only remove after the first click
-    } else {
-    
     // get the user's year from the search box
     var userOneYear = document.getElementById('1yearsearch').value;
 
@@ -43,9 +60,9 @@ function oneYearLoad() {
      viewer_main.scene.globe.depthTestAgainstTerrain = true;
  
          
-         viewer_main.globe = true;
-         viewer_main.scene.globe.enableLighting = true;
-     viewer_main.clock.multiplier =  100 ;               // speed of the simulation
+    viewer_main.globe = true;
+    viewer_main.scene.globe.enableLighting = true;
+    viewer_main.clock.multiplier =  100 ;               // speed of the simulation
      
      var mycredit= new Cesium.Credit("Space Geodesy and Navigation Laboratory",'data/sgnl.png','https://www.ucl.ac.uk');
      // var mycredit = new Cesium.Credit('Cesium', 'data/sgnl.png', 'https://www.ucl.ac.uk');
@@ -60,15 +77,7 @@ function oneYearLoad() {
      viewer_main.clock.clockRange = Cesium.ClockRange.UNBOUNDED;
      viewer_main.timeline.zoomTo(start_jd, Cesium.JulianDate.addSeconds(start_jd, 86400, new Cesium.JulianDate()));
  
-     
- 
-   
-     // var satcat_log = "data/catalogue/fspcat_20230101.json";
-     // var tle_log = "data/tle/geo_tle.json";
- 
-     // satcat.loadcatlog("kep",satcat_log);
-     // satcat.loadcatlog("tle",tle_log);
-     
+          
      /// debris_collection to store all the debris points
      debris_collection = new Cesium.PointPrimitiveCollection();
      debri_collection_radar = new Cesium.PointPrimitiveCollection();
@@ -113,13 +122,16 @@ function oneYearLoad() {
  
            data_load=true;
            // clearInterval(timename); /// clear itself
-         }
+           }
+    
+          //  viewer_main.scene.postUpdate.addEventListener(icrf_view_main); // enable Earth rotation, everything is seen to be in eci
+          //  viewer_main.scene.preRender.addEventListener(update_debris_position);
+          //  viewer_main.scene.postUpdate.addEventListener(update_debris_position);
+    
      },1000); /// allow sometime to load the Earth 
  
-   
      viewer_main.scene.postUpdate.addEventListener(icrf_view_main); // enable Earth rotation, everything is seen to be in eci
-     viewer_main.scene.preRender.addEventListener(update_debris_position);
-     ///viewer_main.scene.preRender.raiseEvent(debris_collection, viewer_main,mycatlog);
-  
+     //viewer_main.scene.preRender.addEventListener(update_debris_position);
+     viewer_main.scene.postUpdate.addEventListener(update_debris_position);
   }
-}
+
